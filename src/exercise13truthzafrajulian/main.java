07ba4df31a9da96ce6208d75c4cf76e819309811
TRUTH <TRUTH@172.22.16.122>
2024-03-05 14:28:21 +0800

    new project repository

---
+++ src/exercise13truthzafrajulian/main.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise13truthzafrajulian;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author TRUTH
+ */
+public class main extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("InitialScreen.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Subject math = new Subject("Math", "math.png", 4, 1.75);
+        Subject bio = new Subject("Biology", "biology.png", 3, 2.0);
+        Subject chem = new Subject("Chemistry", "chemistry.png", 3, 1.5);
+        Subject physics = new Subject("Physics", "physics.png", 3, 1.75);
+        Subject cs = new Subject("CS", "computer science.png", 1, 1.5);
+        
+        Subject displayedSubject = cs;  
+        
+        launch(args);
+        
+    }
+    
+}
